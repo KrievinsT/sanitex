a67f13042565d332e054a104029cb27ac00bdbf9
krievins <toms.krievins@wdmarket.lv>
2025-03-05 13:14:02 +0200

    PRoduct Info

--- download.php
+++ download.php
@@ -3,7 +3,7 @@
     flush();
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['download'])) {
-        $filePath = 'uploads/Products.csv'; // Path to the file
+        $filePath = 'uploads/ProductInfo.csv'; // Path to the file
     
         if (!file_exists($filePath)) {
             die("Error: The file does not exist.");
